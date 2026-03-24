2c4801318c69806278b9f607de6b1e2555d3aa72
Luca Nagel <luca.nagel@tum.de>
2026-03-24 15:18:57 +0100

    release: bump version to 1.0.14

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ copyright = "2025, Luca Nagel"
 author = "Luca Nagel"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.13"
+release = "1.0.14"
 
 
 # -- General configuration ---------------------------------------------------
