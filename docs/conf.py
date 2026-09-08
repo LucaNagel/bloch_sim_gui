2c2311505ee6d14a640e370658db7bc903d1e672
Luca Nagel <luca.nagel@tum.de>
2026-09-08 19:35:01 +0200

    Bump version to 2.6.4

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ copyright = "2025, Luca Nagel"
 author = "Luca Nagel"
 
 # The full version, including alpha/beta/rc tags
-release = "2.6.3"
+release = "2.6.4"
 
 
 # -- General configuration ---------------------------------------------------
